2cadbdb9d7a679d494222e8f40ba330c3adb4ac1
Alexey Kirpichnikov <alex.kirp@gmail.com>
2020-06-26 13:53:13 +0500

    Initial commit

---
+++ app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0] + " " + args[1]);
+        }
+    }
+}
