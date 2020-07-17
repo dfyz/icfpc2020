4890d886dd7b23f80069b1e8aeb0c12ab2cb75d7
Ivan.Komarov <Ivan.Komarov@dfyz.info>
2020-07-17 23:50:35 +0300

    add tests

--- Tests/InterpreterTests.cs
+++ Tests/InterpreterTests.cs
@@ -9,6 +9,15 @@ namespace Test
     public class InterpreterTests
     {
         [Fact]
+        public void TestAdd() {
+            EvalAssert("3", "ap ap add 1 2");
+            EvalAssert("3", "ap ap add 2 1");
+            EvalAssert("1", "ap ap add 0 1");
+            EvalAssert("5", "ap ap add 2 3");
+            EvalAssert("8", "ap ap add 3 5");
+        }
+
+        [Fact]
         public void TestMul()
         {
             EvalAssert("8", "ap ap mul 4 2");
