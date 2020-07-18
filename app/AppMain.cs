aabf9823b3137557dd160760e4305b86bfdacc83
Ivan Korostelev <ivkorost@microsoft.com>
2020-07-18 10:38:44 +0100

    Interactive mode

--- app/AppMain.cs
+++ app/AppMain.cs
@@ -9,7 +9,34 @@ namespace app
         {
             var programText = File.ReadAllText("../../../../galaxy.txt");
             var env = Env.Load(programText);
+            
+            // Iterate00(env);
+            Interactive(env);
+            return 0;
+        }
 
+        private static void Interactive(Env env)
+        {
+            var state = Value.Nil;
+            for (var i = 0;; ++i)
+            {
+                Console.Write("Enter coords: ");
+                var coords = Console.ReadLine().Split(' ');
+                var x = int.Parse(coords[0]);
+                var y = int.Parse(coords[1]);
+
+                var res = env.Eval(
+                    "ap ap ap interact galaxy $1 ap ap vec $2 $3",
+                    state,
+                    new Integer { Val = x },
+                    new Integer { Val = y });
+                state = res.GetFirst();
+                Console.WriteLine(env.Eval("ap multipledraw $1", res.GetSecond()));
+            }
+        }
+
+        private static void Iterate00(Env env)
+        {
             var state = Value.Nil;
             for (var i = 0; i < 10; ++i)
             {
@@ -18,8 +45,6 @@ namespace app
                 state = res.GetFirst();
                 Console.WriteLine(env.Eval("ap multipledraw $1", res.GetSecond()));
             }
-
-            return 0;
         }
     }
 }
